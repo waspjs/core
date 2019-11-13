561880c53939556d29dcd63691f32ca71b8393e1
Alex Hicks <alex@alexhicks.net>
2019-11-12 21:31:46 -0500

    Exclude non-existent JS paths

--- src/service/AssetDirectoryService/AssetDirectoryService.ts
+++ src/service/AssetDirectoryService/AssetDirectoryService.ts
@@ -1,6 +1,7 @@
 import { resolve as resolvePath } from "path";
 import * as _ from "lodash";
 import { Service } from "typedi";
+import { pathExists } from "../../util/pathExists";
 
 @Service()
 export class AssetDirectoryService {
@@ -13,6 +14,8 @@ export class AssetDirectoryService {
       // Get out of "dist" so we can go back into "src"
       resolvePath(__dirname, "../../../src/views/js"),
       resolvePath(process.cwd(), "src/views/js")
-    ]).map(d => d.replace(/\\/g, "/"));
+    ]).map(d =>
+      d.replace(/\\/g, "/")
+    ).filter(d => pathExists(d));
   }
 }
